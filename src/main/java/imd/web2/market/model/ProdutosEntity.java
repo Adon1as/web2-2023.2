d9b9fc5dba432fb4ee0ac3087c310d2e7070a088
adon1as <adonias.dev@gmail.com>
2023-11-21 17:14:57 -0300

    feature: validacao de produto
    #2

--- src/main/java/imd/web2/market/model/ProdutosEntity.java
+++ src/main/java/imd/web2/market/model/ProdutosEntity.java
@@ -1,6 +1,9 @@
 package imd.web2.market.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,17 +15,30 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class produto {
+public class ProdutosEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotNull
+    @Size(min = 1, max = 50, message = "O nome do produto precisar ser preechido, com ate 50 caracteres.")
     private String nome_produto;
+
+    @NotNull
+    @Size(min = 1, max = 50, message = "A  descricao do produto precisar ser preechido, com ate 100 caracteres.")
     private String descricao_produt;
+
     private String preco_produto;
+
+    @Future
     private String data_validade;
+
     private String estoque;
+
+    @Size(min = 1, max = 50, message = "O fornecedor do produto precisar ser preechido, com ate 50 caracteres.")
     private String fornecedor;
+
     private boolean ativo;
 
 }
