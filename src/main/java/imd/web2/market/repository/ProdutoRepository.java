8879fdf645ce1e1be04934eab9e77345d8bec7e2
adon1as <adonias.dev@gmail.com>
2023-11-21 17:35:28 -0300

    feature: criacao do repository de produto
    #10

---
+++ src/main/java/imd/web2/market/repository/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package imd.web2.market.repository;
+
+import imd.web2.market.model.ProdutosEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoRepository extends JpaRepository<ProdutosEntity,Long> {
+
+}
